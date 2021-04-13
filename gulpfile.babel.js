4b3a11dc5aac9196724f37811d741d42a3455dee
ljayG <moguri61@gmail.com>
2021-04-13 13:41:24 +0900

    Dev Server

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,8 @@
 import gulp from 'gulp';
 import gpug from 'gulp-pug';
 import del from 'del';
+import ws from 'gulp-webserver';
+import livereload from 'connect-livereload';
 
 const routes = {
     pug: {
@@ -17,8 +19,18 @@ const pug = () =>
 
 const clean = () => del(['build']);
 
+const webserver = () => 
+    gulp
+        .src('build')
+        .pipe(ws({
+            livereload: true,
+            open: true
+        }))
+
 const prepare = gulp.series([clean]);
 
 const assets = gulp.series([pug]);
 
-export const dev = gulp.series([prepare, assets]); 
+const postDev = gulp.series([webserver]);
+
+export const dev = gulp.series([prepare, assets, postDev]); 
